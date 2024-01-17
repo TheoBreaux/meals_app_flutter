fe784a6264bf03f39e2cb7e86118f62f270b0467
Theo Breaux <theobreaux@gmail.com>
2024-01-17 12:17:02 -0800

    meal item widget

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/data/dummy_data.dart';
+import 'package:meals_app_flutter/models/category.dart';
 import 'package:meals_app_flutter/screens/meals.dart';
 import 'package:meals_app_flutter/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => const MealsScreen(
-          title: 'Some Title',
-          meals: [],
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -37,7 +42,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app_flutter/models/meal.dart';
+import 'package:meals_app_flutter/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -37,12 +38,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => Text(
-          meals[index].title,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        ),
+        itemBuilder: (context, index) => MealItem(meal: meals[index]),
       );
     }
 
